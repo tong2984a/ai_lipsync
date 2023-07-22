a757d4409488ec7f0de368f2123465a076a21dcf
Leung <andrew_leung@cable.comcast.net>
2023-07-22 00:19:44 -0400

    pretty screen

--- hello.py
+++ hello.py
@@ -29,30 +29,22 @@ def display_video(filename):
 @app.route('/uploader', methods = ['GET', 'POST'])
 def upload_file():
 	if request.method == 'POST':
-		#   f = request.files['file']
-		print('args', request.args)
-		files_uploaded = request.files
-		# files_uploaded = request.files.getlist('user_file_images')
-		print('files_uploaded', files_uploaded)
-		audioFile = files_uploaded.getlist('file1')
-		imgFile = files_uploaded.getlist('file')
+		audioFile = request.files['audioFile']
 		print('audioFile', audioFile)
-		print('imgFile', imgFile)
-
-		audioFile = request.files['file1']
 		filename = secure_filename(audioFile.filename) # save file 
 		audioFilepath = os.path.join(app.instance_path, filename);
 		audioFile.save(audioFilepath)
 
-		imgFile = request.files['file']
+		imgFile = request.files['imgFile']
+		print('imgFile', imgFile)
 		filename = secure_filename(imgFile.filename) # save file 
 		imgFilepath = os.path.join(app.instance_path, filename);
 		imgFile.save(imgFilepath)
 
-		outputFilename = UPLOAD_FOLDER + 'result_voice.mp4'
-		inference.makeFace(audioFilepath, imgFilepath, outputFilename)
+		outputFilename = UPLOAD_FOLDER + 'merge.mp4'
+		inference_web.makeFace(audioFilepath, imgFilepath, outputFilename)
 		flash('Video successfully uploaded and displayed below')
-		return render_template('index.html', filename=outputFilename)
+		return render_template('mini-music-player.html', filename=outputFilename)
 	else:
 		print('args', request.args)
 		print('files', request.files)
@@ -60,10 +52,8 @@ def upload_file():
 @app.route('/login', methods=['POST', 'GET'])
 def login():
 	if request.method == 'POST':
-###		print('user', user, 'file', file, 'audiofile', audiofile)
 		print(request.form)
 		user = request.form['nm']
-###		file = request.files['file']
 		audiofile = request.files['audiofile']
 		return redirect(url_for('success', name=user, audiofile=audiofile))
 	else:
